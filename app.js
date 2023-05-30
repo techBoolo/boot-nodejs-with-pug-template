cf015264b643df5be7e427d29c5809cae699b24b
teklu <teklufa@gmail.com>
2023-05-30 13:03:29 +0300

    use express to handle requests
    install express and create an app to handle requests and pass the handler to our http server

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.use((req, res) => {
+  res.status(200).json({
+    message: 'it works'
+  })
+})
+
+export default app
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import http from 'node:http'
+import app from './app.js'
 
 const PORT = process.env.PORT || 3001
 const server = http.createServer()
 
-server.on('request', (req, res) => {
-
-  res.end('it works')
-})
+server.on('request', app)
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
