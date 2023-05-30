8c6ca8a3fa7e9c30810a867a667d4d28c3d8875f
teklu <teklufa@gmail.com>
2023-05-30 15:08:00 +0300

    use pug template engine
    install and setup pug engine to use it as a template engine
    test by rendering an index page for a user requesting for root url
    include in the express app to serve static files from ./public folder

--- app.js
+++ app.js
@@ -2,21 +2,13 @@ import express from 'express'
 import routeNotFound from './middlewares/route_not_found.js'
 import errorHandler from './middlewares/error_handler.js'
 
-import { getDB } from './config/db.js'
-
 const app = express()
+app.set('views', './views')
+app.set('view engine', 'pug')
 
+app.use(express.static('public'))
 app.get('/', async (req, res) => {
-  res.status(200).json({
-    message: 'it works'
-  })
-})
-
-app.get('/users', async (req, res) => {
-  const User = getDB().collection('users') 
-  const cursor =  User.find()
-  const users = await cursor.toArray()
-  res.status(200).json(users)
+  res.render('index')
 })
 
 app.use(routeNotFound)
